93260398c34ae86a1577a920a90f37605fa6f6f7
João Carlos Barrozo <joaocbarrozo@gmail.com>
2024-12-05 09:31:31 -0300

    Refatoração das funções que calculavam as estatisticas e finaliação do jogos.JSON

--- script.js
+++
@@ -1,5 +0,0 @@
-let btnFg = document.getElementById("field-goal-btn");
-let eventText = document.getElementById("event-list-title")
-btnFg.addEventListener("click",() => {
-    eventText.textContent = "Game";
-})
